e54c8ae63381e0824f2621927d1c15fe8f30928e
Nikita Patil <patilnikita296@gmail.com>
2025-03-13 16:08:11 +0100

    Different approaches of import madules and sys.path for module location track

--- ImportModule/intro.py
+++ ImportModule/intro.py
@@ -1,4 +1,22 @@
+#import my_module
+#import my_module as mod
+#from my_module import find_index , test
+'''Hard to track where the function or variable came from '''
+from my_module import * 
 
-import my_module
+'''How to find module location:
+1. script location
+2. python path environment variable
+3. standard library directory
+4. site packages directory for third party packages'''
+import sys
+
 
 cources = ['History','math','physics','compSci']
+
+# index = my_module.find_index(cources,'physics')
+# index = mod.find_index(cources,'physics')
+index = find_index(cources,'physics')
+#print(index)
+#print(test)
+print(sys.path)
