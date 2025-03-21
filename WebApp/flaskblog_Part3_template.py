b484638f63323b555dc7d795943c0322d9067cbb
Nikita Patil <patilnikita296@gmail.com>
2025-03-21 18:20:51 +0100

    Added flaskblog with template inheritance

---
+++ WebApp/flaskblog_Part3_template.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Nikita Patil',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2025'
+    },
+    {
+        'author': 'Rohan More',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2025'
+    }
+]
+
+@app.route("/")
+def home():
+    return render_template('home.html',posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html',title='AboutPage')
+
+if __name__ == '__main__':
+    app.run(debug=True)
