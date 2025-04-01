0fe5252b03aff1833b9999b473c815e48ef69a6a
Nikita Patil <patilnikita296@gmail.com>
2025-04-01 15:28:55 +0200

    Added user authentication

--- WebApp_User_Authentication/flaskblog/__init__.py
+++ WebApp_User_Authentication/flaskblog/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+migrate = Migrate(app, db)
 
 from flaskblog import routes
