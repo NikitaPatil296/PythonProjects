8affefb5f34dead14e02c71a5145963a0f646aee
Nikita Patil <patilnikita296@gmail.com>
2025-04-19 17:47:31 +0200

    classmethod and staticmethod

--- Python_OOP/Part2_Class_Variables.py
+++ Python_OOP/Part2_Class_Variables.py
@@ -1,12 +1,15 @@
 class Employee:
 
-    raise_amount = 1.04     # Class variable
+    raise_amount = 1.04     # class variable
+    no_of_emps = 0          # class variable
 
     def __init__(self, first, last,pay):
         self.first = first                                                 # Instance variables
         self.last = last
         self.pay = pay
         self.email = first + '.' + last + '@company.com'
+
+        Employee.no_of_emps += 1
 
     def fullName(self):
         return '{} {}'.format(emp1.first, emp1.last)
@@ -17,6 +20,8 @@ class Employee:
 
 emp1 = Employee('Rohan','More',50000)
 emp2 = Employee('Nikita','Patil',50000)
+
+print(Employee.no_of_emps)
 
 # emp1.raise_amount = 2
 # print(emp1.__dict__)              # Namespace of emp1
