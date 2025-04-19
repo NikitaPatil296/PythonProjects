88bfb59254d33dc2e2c3963bae451786d46f324a
Nikita Patil <patilnikita296@gmail.com>
2025-04-19 17:08:44 +0200

    Python OOP

---
+++ Python_OOP/Part2_Class_Variables.py
@@ -0,0 +1,23 @@
+class Employee:
+
+    raise_amount = 1.04     # Class variable
+
+    def __init__(self, first, last,pay):
+        self.first = first                                                 # Instance variables
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullName(self):
+        return '{} {}'.format(emp1.first, emp1.last)
+    
+    def apply_raise(self):
+        self.pay =  int(self.pay * self.raise_amount)
+
+
+emp1 = Employee('Rohan','More',50000)
+emp2 = Employee('Nikita','Patil',50000)
+
+# emp1.raise_amount = 2
+# print(emp1.__dict__)              # Namespace of emp1
+# print(Employee.__dict__)
